d5d38b696005366c55d03a733277ad620af83678
SAJacob7 <sophiajacob@ku.edu>
2025-04-19 17:05:28 -0500

    Added more queries

--- test.sql
+++ test.sql
@@ -1,7 +1,29 @@
--- 11.
-SELECT lm.type_id AS membership_type,
-    SUM(p.amount_paid) AS total_fees_collected
-FROM Pay p
-    JOIN Library_Member lm ON p.member_id = lm.member_id
-WHERE p.paid_date >= (CURDATE() - 30)
-GROUP BY lm.type_id;
+WITH total_fantasy_recent_checked_out AS (
+    SELECT lm.member_id,
+        lm.name,
+        COUNT(c.copy_id) AS total_checked_out
+    FROM Library_Member lm
+        JOIN Make m ON lm.member_id = m.member_id
+        JOIN Library_Transaction lt ON m.transaction_id = lt.transaction_id
+        JOIN Loan l ON lt.transaction_id = l.transaction_id
+        JOIN Copy c ON l.copy_id = c.copy_id
+        JOIN Originate o ON c.copy_id = o.copy_id
+        JOIN Book bk ON o.item_id = bk.item_id
+    WHERE bk.genre = "Fantasy"
+        AND (
+            lt.checked_out_date BETWEEN DATE_FORMAT(
+                DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR),
+                '%Y-01-01'
+            )
+            AND DATE_FORMAT(
+                DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR),
+                '%Y-12-31'
+            )
+        )
+    GROUP BY lm.member_id,
+        lm.name
+)
+SELECT member_id,
+    name,
+    MAX(total_checked_out)
+FROM total_fantasy_recent_checked_out tf;
